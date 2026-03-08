1d69882d9bf7219327e2a7857cf1e9ef89d9efdb
Jorge Cueva <jmoics@gmail.com>
2026-03-08 17:23:06 -0500

    Create a channel in Spring Integration.

--- dashboard/src/main/java/com/lil/springintegration/manage/DashboardManager.java
+++ dashboard/src/main/java/com/lil/springintegration/manage/DashboardManager.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.integration.channel.AbstractSubscribableChannel;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.channel.PublishSubscribeChannel;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
@@ -33,7 +34,9 @@ public class DashboardManager {
         initializePowerUsage();
     }
 
-    public static ClassPathXmlApplicationContext getDashboardContext() { return (ClassPathXmlApplicationContext) DashboardManager.context; }
+    public static ClassPathXmlApplicationContext getDashboardContext() {
+        return (ClassPathXmlApplicationContext) DashboardManager.context;
+    }
 
     static void setDashboardStatus(String key, String value) {
         String v = (value != null ? value : "");
@@ -62,7 +65,9 @@ public class DashboardManager {
                 .build();
 
         // Now, to send our message, we need a channel!
-        //AbstractSubscribableChannel techSupportChannel =
+        // DirectChannel is a synchronous single-threaded communication pipe
+        AbstractSubscribableChannel techSupportChannel = (DirectChannel) DashboardManager.getDashboardContext().getBean("techSupportChannel");
+        techSupportChannel.send(message);
     }
 
     private void initializeGridStatus() {
